c9d284f718ac8189bfc015d536a883b4ca3d7ceb
Cole Carley <cole.carley@gmail.com>
2025-02-20 06:28:50 +0000

    visit const stmt

--- include/visitors/code_gen.h
+++ include/visitors/code_gen.h
@@ -214,41 +214,7 @@ public:
     void visit_unary(Unary *unary);
     void visit_primary(Primary *primary);
     void visit_ternary(Ternary *ternary);
-
-    void visit_const_stmt(ConstStmt *const_stmt)
-    {
-        const_stmt->value->accept(this);
-        TaggedExpression initializer = this->stack.pop();
-
-        std::shared_ptr<BirdType> type;
-        if (const_stmt->type.has_value())
-        {
-            type = this->type_converter.convert(const_stmt->type.value());
-        }
-        else
-        {
-            if (initializer.type->type != BirdTypeType::VOID)
-            {
-                type = initializer.type;
-            }
-            else
-            {
-                BinaryenType binaryen_type = BinaryenExpressionGetType(initializer.value);
-                type = (binaryen_type == BinaryenTypeFloat64())
-                           ? std::shared_ptr<BirdType>(new FloatType())
-                           : std::shared_ptr<BirdType>(new IntType());
-            }
-        }
-
-        BinaryenIndex index = this->function_locals[this->current_function_name].size();
-        this->function_locals[this->current_function_name].push_back(from_bird_type(type));
-
-        environment.declare(const_stmt->identifier.lexeme, TaggedIndex(index, type));
-
-        BinaryenExpressionRef set_local = this->binaryen_set(const_stmt->identifier.lexeme, initializer.value);
-
-        this->stack.push(TaggedExpression(set_local, type));
-    }
+    void visit_const_stmt(ConstStmt *const_stmt);
 
     void visit_func(Func *func)
     {
