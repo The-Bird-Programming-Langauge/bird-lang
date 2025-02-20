2ecf765c6ef99cfbad3fbfcea4d49d1d4831600e
Cole Carley <cole.carley@gmail.com>
2025-02-20 04:15:31 +0000

    separate add_memory_segment

--- include/visitors/code_gen.h
+++ include/visitors/code_gen.h
@@ -155,25 +155,7 @@ public:
 
     void init_std_lib();
 
-    void add_memory_segment(const std::string &str)
-    {
-        if (this->str_offsets.find(str) != this->str_offsets.end())
-        {
-            return;
-        }
-
-        auto str_offset = this->current_offset;
-        this->current_offset += str.size() + 1;
-
-        MemorySegment segment = {
-            str.c_str(),
-            static_cast<BinaryenIndex>(str.size() + 1), // + 1 for '\0'
-            BinaryenConst(this->mod, BinaryenLiteralInt32(str_offset))};
-
-        this->str_offsets[str] = str_offset;
-
-        this->memory_segments.push_back(segment);
-    }
+    void add_memory_segment(const std::string &str);
 
     void init_static_memory(std::vector<std::string> &strings)
     {
