5be4ccf37cd398a2e55a2aefc30fa2d9101aadef
Cole Carley <cole.carley@gmail.com>
2025-02-20 06:35:33 +0000

    visit subscript

--- include/visitors/code_gen.h
+++ include/visitors/code_gen.h
@@ -222,43 +222,7 @@ public:
     void visit_break_stmt(BreakStmt *break_stmt);
     void visit_continue_stmt(ContinueStmt *continue_stmt);
     void visit_type_stmt(TypeStmt *type_stmt);
-
-    void visit_subscript(Subscript *subscript)
-    {
-        subscript->subscriptable->accept(this);
-        auto subscriptable = this->stack.pop();
-
-        subscript->index->accept(this);
-        auto index = this->stack.pop();
-
-        std::shared_ptr<BirdType> type;
-        if (subscriptable.type->type == BirdTypeType::ARRAY)
-        {
-            type = safe_dynamic_pointer_cast<ArrayType>(subscriptable.type)->element_type;
-        }
-
-        // TODO: make this better
-        BinaryenExpressionRef args[2] = {subscriptable.value,
-                                         subscriptable.type->type == BirdTypeType::ARRAY ? BinaryenBinary(
-                                                                                               this->mod,
-                                                                                               BinaryenMulInt32(),
-                                                                                               index.value,
-                                                                                               BinaryenConst(
-                                                                                                   this->mod,
-                                                                                                   BinaryenLiteralInt32(bird_type_byte_size(type))))
-                                                                                         : index.value};
-
-        this->stack.push(
-            TaggedExpression(
-                BinaryenCall(
-                    this->mod,
-                    type->type == BirdTypeType::FLOAT ? "mem_get_64"
-                                                      : "mem_get_32",
-                    args,
-                    2,
-                    BinaryenTypeInt32()),
-                std::shared_ptr<BirdType>(type)));
-    }
+    void visit_subscript(Subscript *subscript);
 
     /*
     This function is called when a struct declaration is encountered.
