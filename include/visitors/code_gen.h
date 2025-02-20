153a1052d19496dd4b87dd8ff7a3829023fd37f9
Cole Carley <cole.carley@gmail.com>
2025-02-20 06:18:35 +0000

    visit while stmt

--- include/visitors/code_gen.h
+++ include/visitors/code_gen.h
@@ -206,61 +206,7 @@ public:
     void visit_assign_expr(AssignExpr *assign_expr);
     void visit_print_stmt(PrintStmt *print_stmt);
     void visit_expr_stmt(ExprStmt *expr_stmt);
-
-    void visit_while_stmt(WhileStmt *while_stmt)
-    {
-        std::vector<BinaryenExpressionRef> children;
-
-        while_stmt->stmt->accept(this);
-        TaggedExpression body = this->stack.pop();
-
-        children.push_back(body.value);
-
-        while_stmt->condition->accept(this);
-        TaggedExpression condition = this->stack.pop();
-
-        auto outer_body =
-            BinaryenBlock(
-                this->mod,
-                "BODY",
-                children.data(),
-                children.size(),
-                BinaryenTypeNone());
-
-        std::vector<BinaryenExpressionRef> while_body_children;
-        while_body_children.push_back(outer_body);
-        while_body_children.push_back(BinaryenBreak(
-            this->mod,
-            "LOOP",
-            condition.value,
-            nullptr));
-
-        auto while_body =
-            BinaryenBlock(
-                this->mod,
-                "while_block",
-                while_body_children.data(),
-                while_body_children.size(),
-                BinaryenTypeNone());
-
-        auto if_cond =
-            BinaryenIf(
-                this->mod,
-                condition.value,
-                BinaryenLoop(
-                    this->mod,
-                    "LOOP",
-                    while_body),
-                nullptr);
-
-        this->stack.push(
-            BinaryenBlock(
-                this->mod,
-                "EXIT",
-                &if_cond,
-                1,
-                BinaryenTypeNone()));
-    }
+    void visit_while_stmt(WhileStmt *while_stmt);
 
     void visit_for_stmt(ForStmt *for_stmt)
     {
