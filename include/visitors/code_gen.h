543f23bf4de5038ac10a3819c5727042c9abe8a0
Cole Carley <cole.carley@gmail.com>
2025-02-20 04:12:40 +0000

    separte init std lib

--- include/visitors/code_gen.h
+++ include/visitors/code_gen.h
@@ -8,6 +8,8 @@
 #include <ios>
 #include <map>
 #include "type_converter.h"
+#include "stack.h"
+#include "sym_table.h"
 
 static unsigned int bird_type_byte_size(std::shared_ptr<BirdType> type) // in i32s
 {
@@ -151,128 +153,7 @@ public:
         this->type_table.push_env();
     }
 
-    void init_std_lib()
-    {
-        BinaryenAddFunctionImport(
-            this->mod,
-            "print_i32",
-            "env",
-            "print_i32",
-            BinaryenTypeInt32(),
-            BinaryenTypeNone());
-
-        BinaryenAddFunctionImport(
-            this->mod,
-            "print_f64",
-            "env",
-            "print_f64",
-            BinaryenTypeFloat64(),
-            BinaryenTypeNone());
-
-        
-            BinaryenAddFunctionImport(
-                this->mod,
-                "print_bool",
-                "env",
-                "print_bool",
-                BinaryenTypeInt32(),
-                BinaryenTypeNone());
-
-        BinaryenAddFunctionImport(
-            this->mod,
-            "print_str",
-            "env",
-            "print_str",
-            BinaryenTypeInt32(),
-            BinaryenTypeNone());
-
-        BinaryenAddFunctionImport(
-            this->mod,
-            "print_endline",
-            "env",
-            "print_endline",
-            BinaryenTypeNone(),
-            BinaryenTypeNone());
-
-        BinaryenType args[2] = {BinaryenTypeInt32(), BinaryenTypeInt32()}; // pointer and index
-        BinaryenType args_type = BinaryenTypeCreate(args, 2);
-        BinaryenAddFunctionImport(
-            this->mod,
-            "mem_get_32",
-            "env",
-            "mem_get_32",
-            args_type,
-            BinaryenTypeInt32());
-
-        BinaryenAddFunctionImport(
-            this->mod,
-            "mem_get_64",
-            "env",
-            "mem_get_64",
-            args_type,
-            BinaryenTypeFloat64());
-
-        // mem_set(pointer, index, value)
-        BinaryenType args_set[3] = {BinaryenTypeInt32(), BinaryenTypeInt32(), BinaryenTypeInt32()};
-        BinaryenType args_set_type = BinaryenTypeCreate(args_set, 3);
-        BinaryenAddFunctionImport(
-            this->mod,
-            "mem_set_32",
-            "env",
-            "mem_set_32",
-            args_set_type,
-            BinaryenTypeNone());
-
-        BinaryenAddFunctionImport(
-            this->mod,
-            "mem_set_ptr",
-            "env",
-            "mem_set_ptr",
-            args_set_type,
-            BinaryenTypeNone());
-
-        BinaryenAddFunctionImport(
-            this->mod,
-            "initialize_memory",
-            "env",
-            "initialize_memory",
-            BinaryenTypeInt32(),
-            BinaryenTypeNone());
-
-        BinaryenType args_set_64[3] = {BinaryenTypeInt32(), BinaryenTypeInt32(), BinaryenTypeFloat64()};
-        BinaryenType args_set_64_type = BinaryenTypeCreate(args_set_64, 3);
-        BinaryenAddFunctionImport(
-            this->mod,
-            "mem_set_64",
-            "env",
-            "mem_set_64",
-            args_set_64_type,
-            BinaryenTypeNone());
-
-        BinaryenAddFunctionImport(
-            this->mod,
-            "mem_alloc",
-            "env",
-            "mem_alloc",
-            BinaryenTypeInt32(),
-            BinaryenTypeInt32());
-
-        BinaryenAddFunctionImport(
-            this->mod,
-            "mark",
-            "env",
-            "mark",
-            BinaryenTypeInt32(),
-            BinaryenTypeNone());
-
-        BinaryenAddFunctionImport(
-            this->mod,
-            "sweep",
-            "env",
-            "sweep",
-            BinaryenTypeNone(),
-            BinaryenTypeNone());
-    }
+    void init_std_lib();
 
     void add_memory_segment(const std::string &str)
     {
---
+++ src/visitors/code_gen/init_std_lib.cpp
@@ -0,0 +1,123 @@
+#include "visitors/code_gen.h"
+
+void CodeGen::init_std_lib()
+{
+    BinaryenAddFunctionImport(
+        this->mod,
+        "print_i32",
+        "env",
+        "print_i32",
+        BinaryenTypeInt32(),
+        BinaryenTypeNone());
+
+    BinaryenAddFunctionImport(
+        this->mod,
+        "print_f64",
+        "env",
+        "print_f64",
+        BinaryenTypeFloat64(),
+        BinaryenTypeNone());
+
+    BinaryenAddFunctionImport(
+        this->mod,
+        "print_bool",
+        "env",
+        "print_bool",
+        BinaryenTypeInt32(),
+        BinaryenTypeNone());
+
+    BinaryenAddFunctionImport(
+        this->mod,
+        "print_str",
+        "env",
+        "print_str",
+        BinaryenTypeInt32(),
+        BinaryenTypeNone());
+
+    BinaryenAddFunctionImport(
+        this->mod,
+        "print_endline",
+        "env",
+        "print_endline",
+        BinaryenTypeNone(),
+        BinaryenTypeNone());
+
+    BinaryenType args[2] = {BinaryenTypeInt32(), BinaryenTypeInt32()}; // pointer and index
+    BinaryenType args_type = BinaryenTypeCreate(args, 2);
+    BinaryenAddFunctionImport(
+        this->mod,
+        "mem_get_32",
+        "env",
+        "mem_get_32",
+        args_type,
+        BinaryenTypeInt32());
+
+    BinaryenAddFunctionImport(
+        this->mod,
+        "mem_get_64",
+        "env",
+        "mem_get_64",
+        args_type,
+        BinaryenTypeFloat64());
+
+    // mem_set(pointer, index, value)
+    BinaryenType args_set[3] = {BinaryenTypeInt32(), BinaryenTypeInt32(), BinaryenTypeInt32()};
+    BinaryenType args_set_type = BinaryenTypeCreate(args_set, 3);
+    BinaryenAddFunctionImport(
+        this->mod,
+        "mem_set_32",
+        "env",
+        "mem_set_32",
+        args_set_type,
+        BinaryenTypeNone());
+
+    BinaryenAddFunctionImport(
+        this->mod,
+        "mem_set_ptr",
+        "env",
+        "mem_set_ptr",
+        args_set_type,
+        BinaryenTypeNone());
+
+    BinaryenAddFunctionImport(
+        this->mod,
+        "initialize_memory",
+        "env",
+        "initialize_memory",
+        BinaryenTypeInt32(),
+        BinaryenTypeNone());
+
+    BinaryenType args_set_64[3] = {BinaryenTypeInt32(), BinaryenTypeInt32(), BinaryenTypeFloat64()};
+    BinaryenType args_set_64_type = BinaryenTypeCreate(args_set_64, 3);
+    BinaryenAddFunctionImport(
+        this->mod,
+        "mem_set_64",
+        "env",
+        "mem_set_64",
+        args_set_64_type,
+        BinaryenTypeNone());
+
+    BinaryenAddFunctionImport(
+        this->mod,
+        "mem_alloc",
+        "env",
+        "mem_alloc",
+        BinaryenTypeInt32(),
+        BinaryenTypeInt32());
+
+    BinaryenAddFunctionImport(
+        this->mod,
+        "mark",
+        "env",
+        "mark",
+        BinaryenTypeInt32(),
+        BinaryenTypeNone());
+
+    BinaryenAddFunctionImport(
+        this->mod,
+        "sweep",
+        "env",
+        "sweep",
+        BinaryenTypeNone(),
+        BinaryenTypeNone());
+}
