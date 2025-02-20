63a7112ced41745f300d4738b9c89f57537ca41b
Cole Carley <cole.carley@gmail.com>
2025-02-20 06:31:29 +0000

    visit call

--- include/visitors/code_gen.h
+++ include/visitors/code_gen.h
@@ -217,29 +217,7 @@ public:
     void visit_const_stmt(ConstStmt *const_stmt);
     void visit_func(Func *func);
     void visit_if_stmt(IfStmt *if_stmt);
-
-    void visit_call(Call *call)
-    {
-        auto func_name = call->identifier.lexeme;
-
-        std::vector<BinaryenExpressionRef> args;
-
-        for (auto &arg : call->args)
-        {
-            arg->accept(this);
-            args.push_back(this->stack.pop().value);
-        }
-
-        auto return_type = this->function_return_types[func_name];
-        this->stack.push(TaggedExpression(
-            BinaryenCall(
-                this->mod,
-                func_name.c_str(),
-                args.data(),
-                args.size(),
-                return_type.value),
-            return_type.type));
-    }
+    void visit_call(Call *call);
 
     void visit_return_stmt(ReturnStmt *return_stmt)
     {
