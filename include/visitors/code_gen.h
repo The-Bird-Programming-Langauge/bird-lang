5a788232e9c6ed573918b9c856daa00b6912c0ed
Cole Carley <cole.carley@gmail.com>
2025-02-20 06:39:28 +0000

    visit as cast

--- include/visitors/code_gen.h
+++ include/visitors/code_gen.h
@@ -235,39 +235,7 @@ public:
     std::unordered_map<std::string, std::string> struct_constructors;
     void visit_struct_initialization(StructInitialization *struct_initialization);
     void visit_member_assign(MemberAssign *member_assign);
-
-    void visit_as_cast(AsCast *as_cast)
-    {
-        as_cast->expr->accept(this);
-        auto expr = this->stack.pop();
-
-        std::shared_ptr<BirdType> to_type = this->type_converter.convert(as_cast->type);
-
-        if (to_type->type == BirdTypeType::INT && expr.type->type == BirdTypeType::FLOAT)
-        {
-            this->stack.push(
-                TaggedExpression(
-                    BinaryenUnary(
-                        this->mod,
-                        BinaryenTruncSatSFloat64ToInt32(),
-                        expr.value),
-                    std::shared_ptr<BirdType>(new IntType())));
-            return;
-        }
-        else if (to_type->type == BirdTypeType::FLOAT && expr.type->type == BirdTypeType::INT)
-        {
-            this->stack.push(
-                TaggedExpression(
-                    BinaryenUnary(
-                        this->mod,
-                        BinaryenConvertSInt32ToFloat64(),
-                        expr.value),
-                    std::shared_ptr<BirdType>(new FloatType())));
-            return;
-        }
-
-        this->stack.push(expr);
-    }
+    void visit_as_cast(AsCast *as_cast);
 
     BinaryenExpressionRef binaryen_set(std::string identifier, BinaryenExpressionRef value)
     {
