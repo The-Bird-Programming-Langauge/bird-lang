6ae75c4d0deb8aa44858eee3c08384290fc20a4e
Cole Carley <cole.carley@gmail.com>
2025-02-20 06:36:33 +0000

    visit struct decl

--- include/visitors/code_gen.h
+++ include/visitors/code_gen.h
@@ -228,14 +228,7 @@ public:
     This function is called when a struct declaration is encountered.
     The struct's shape is stored in the type table.
     */
-    void visit_struct_decl(StructDecl *struct_decl)
-    {
-        std::vector<std::pair<std::string, std::shared_ptr<BirdType>>> struct_fields;
-        std::transform(struct_decl->fields.begin(), struct_decl->fields.end(), std::back_inserter(struct_fields), [&](std::pair<std::string, std::shared_ptr<ParseType::Type>> field)
-                       { return std::make_pair(field.first, this->type_converter.convert(field.second)); });
-
-        type_table.declare(struct_decl->identifier.lexeme, std::make_shared<StructType>(struct_decl->identifier.lexeme, struct_fields));
-    }
+    void visit_struct_decl(StructDecl *struct_decl);
 
     void visit_direct_member_access(DirectMemberAccess *direct_member_access)
     {
