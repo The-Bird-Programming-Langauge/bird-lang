e67aca6d157db9be08d2e65acdf1f29fe9b05e72
Nicholas Langley <nlangley@unr.edu>
2025-02-25 23:39:42 +0000

    upper bounds checked for array access, no error is thrown yet

--- src/visitors/code_gen/visit_subscript.cpp
+++ src/visitors/code_gen/visit_subscript.cpp
@@ -1,4 +1,14 @@
 #include "../../../include/visitors/code_gen.h"
+#include <binaryen-c.h>
+#include <memory>
+
+BinaryenExpressionRef
+get_length_binaryen(BinaryenModuleRef mod,
+                    Tagged<BinaryenExpressionRef> &subscriptable);
+
+BinaryenExpressionRef get_index_mem_binaryen(
+    BinaryenModuleRef mod, Tagged<BinaryenExpressionRef> &subscriptable,
+    Tagged<BinaryenExpressionRef> &index, std::shared_ptr<BirdType> type);
 
 void CodeGen::visit_subscript(Subscript *subscript) {
   subscript->subscriptable->accept(this);
@@ -14,21 +24,40 @@ void CodeGen::visit_subscript(Subscript *subscript) {
   std::shared_ptr<BirdType> type =
       safe_dynamic_pointer_cast<ArrayType>(subscriptable.type)->element_type;
 
-  // TODO: make this better
-  BinaryenExpressionRef args[2] = {
-      subscriptable.value,
-      BinaryenBinary(
-          this->mod, BinaryenAddInt32(),
-          BinaryenBinary(
-              this->mod, BinaryenMulInt32(), index.value,
-              BinaryenConst(this->mod,
-                            BinaryenLiteralInt32(bird_type_byte_size(type)))),
-          BinaryenConst(this->mod, BinaryenLiteralInt32(5)))};
+  auto array_length = get_length_binaryen(this->mod, subscriptable);
 
-  this->stack.push(TaggedExpression(
-      BinaryenCall(this->mod,
-                   type->type == BirdTypeType::FLOAT ? "mem_get_64"
-                                                     : "mem_get_32",
-                   args, 2, BinaryenTypeInt32()),
-      std::shared_ptr<BirdType>(type)));
+  BinaryenExpressionRef bounds_checked_access = BinaryenIf(
+      this->mod,
+      BinaryenBinary(this->mod, BinaryenLtSInt32(), index.value, array_length),
+      get_index_mem_binaryen(this->mod, subscriptable, index, type),
+      // BinaryenThrow(this->mod, "Error: index out of bounds", {}, 0));
+      BinaryenConst(this->mod, BinaryenLiteralInt32(0)));
+
+  this->stack.push(
+      TaggedExpression(bounds_checked_access, std::shared_ptr<BirdType>(type)));
+}
+
+BinaryenExpressionRef
+get_length_binaryen(BinaryenModuleRef mod,
+                    Tagged<BinaryenExpressionRef> &subscriptable) {
+  BinaryenExpressionRef mem_get_args[2] = {
+      subscriptable.value, BinaryenConst(mod, BinaryenLiteralInt32(0))};
+  return BinaryenCall(mod, "mem_get_32", mem_get_args, 2, BinaryenTypeInt32());
+}
+
+BinaryenExpressionRef get_index_mem_binaryen(
+    BinaryenModuleRef mod, Tagged<BinaryenExpressionRef> &subscriptable,
+    Tagged<BinaryenExpressionRef> &index, std::shared_ptr<BirdType> type) {
+
+  BinaryenExpressionRef mem_position = BinaryenBinary(
+      mod, BinaryenAddInt32(),
+      BinaryenBinary(
+          mod, BinaryenMulInt32(), index.value,
+          BinaryenConst(mod, BinaryenLiteralInt32(bird_type_byte_size(type)))),
+      BinaryenConst(mod, BinaryenLiteralInt32(5)));
+
+  BinaryenExpressionRef mem_get_args[2] = {subscriptable.value, mem_position};
+  return BinaryenCall(
+      mod, type->type == BirdTypeType::FLOAT ? "mem_get_64" : "mem_get_32",
+      mem_get_args, 2, BinaryenTypeInt32());
 }
