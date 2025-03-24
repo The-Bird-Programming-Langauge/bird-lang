7acb96621d4c27888c54f685fb43edf6da8a2b85
Cole Carley <cole.carley@gmail.com>
2025-03-24 19:40:45 +0000

    fix lambda grammar

--- tests/first_class_functions_test_suite/return_function_test.cpp
+++ tests/first_class_functions_test_suite/return_function_test.cpp
@@ -2,14 +2,14 @@
 
 TEST(FirstClassFunctionReturnTest, WithParams) {
   BirdTest::TestOptions options;
-  options.code = "fn foo(x:(int)int) -> (int)int {return x;}";
+  options.code = "fn foo(x: fn(int) -> int) -> fn(int) -> int {return x;}";
 
   ASSERT_TRUE(BirdTest::compile(options));
 }
 
 TEST(FirstClassFunctionReturnTest, UseReturnedFunction) {
   BirdTest::TestOptions options;
-  options.code = "fn foo(x: (int)int) -> (int)int {return x;}"
+  options.code = "fn foo(x: fn(int) -> int) -> fn(int) -> int {return x;}"
                  "const bar = foo(fn(x: int) -> int { return x; });"
                  "const result = bar(3);"
                  "print result;";
@@ -19,7 +19,8 @@ TEST(FirstClassFunctionReturnTest, UseReturnedFunction) {
 
 TEST(FirstClassFunctionReturnTest, ReturnMismatchFunction) {
   BirdTest::TestOptions options;
-  options.code = "fn foo(bar: (int)void) -> (void)int { return bar; }";
+  options.code =
+      "fn foo(bar: fn(int) -> void) -> fn(void) -> int { return bar; }";
 
   ASSERT_FALSE(BirdTest::compile(options));
 }
