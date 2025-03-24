3142f972b515a73a19c2fdad6b6b8a7e3a91948f
Cole Carley <cole.carley@gmail.com>
2025-03-24 19:51:24 +0000

    added closure tests

---
+++ tests/first_class_functions_test_suite/closure_test.cpp
@@ -0,0 +1,23 @@
+#include "../helpers/compile_helper.hpp"
+
+TEST(FirstClassFunctionParamTest, GoodClosure) {
+  BirdTest::TestOptions options;
+  options.code =
+      "fn foo() -> fn() -> int { var x = 1; return fn() -> int {return x;}; }"
+      "foo()();"
+      "foo()();"
+      "foo()();";
+  ASSERT_TRUE(BirdTest::compile(options));
+}
+
+TEST(FirstClassFunctionParamTest, ClosureWithShadow) {
+  BirdTest::TestOptions options;
+  options.code =
+      "fn foo() -> fn() -> int { var x = 1; return fn() -> int {return x;}; }"
+      "var x = 69;"
+      "foo()();"
+      "foo()();"
+      "foo()();";
+
+  ASSERT_TRUE(BirdTest::compile(options));
+}
