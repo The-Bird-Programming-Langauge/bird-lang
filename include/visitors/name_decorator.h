8ef33449ed1aa9523a2244f439bebcbe3053671e
nathanwrightUNR <nathanwright@nevada.unr.edu>
2025-03-31 21:53:38 +0000

    fixing includes and best practices

--- include/visitors/name_decorator.h
+++ include/visitors/name_decorator.h
@@ -1,20 +1,17 @@
 
 #pragma once
-#include "../ast_node/index.h"
-#include "../exceptions/bird_exception.h"
 #include "../stack.h"
 #include "visitor.h"
 #include "visitor_adapter.h"
 #include <memory>
-#include <set>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
 class NameDecorator : public VisitorAdapter {
+  std::unordered_map<std::string, bool> seen;
   Stack<std::string> ns_stack;
   Stack<std::string> r_stack;
-  std::unordered_map<std::string, bool> seen;
 
 public:
   void decorate(std::vector<std::unique_ptr<Stmt>> *stmts) {
@@ -30,19 +27,19 @@ public:
 
   std::string resolve_identifier(std::string &identifier) {
     // check local namespace for a scoped variable
-    auto from_local = get_current_namespace_prefix() +
-                      get_current_scope_resolution_prefix() + identifier;
-    if (seen[from_local])
+    auto from_local = this->get_current_namespace_prefix() +
+                      this->get_current_scope_resolution_prefix() + identifier;
+    if (this->seen[from_local])
       return from_local;
 
     // check if it is fully qualified as-is
-    auto from_r = get_current_scope_resolution_prefix() + identifier;
-    if (seen[from_r])
+    auto from_r = this->get_current_scope_resolution_prefix() + identifier;
+    if (this->seen[from_r])
       return from_r;
 
     // check local namespace
-    auto from_ns = get_current_namespace_prefix() + identifier;
-    if (seen[from_ns])
+    auto from_ns = this->get_current_namespace_prefix() + identifier;
+    if (this->seen[from_ns])
       return from_ns;
 
     // if we make it here, it is declared outside of the namespace or not fully
@@ -50,10 +47,9 @@ public:
     std::vector<std::string> popped;
     std::string global;
     while (!ns_stack.empty()) {
-      auto top = ns_stack.pop();
-      popped.push_back(top);
-      if (seen[get_current_namespace_prefix() + identifier]) {
-        global = get_current_namespace_prefix() + identifier;
+      popped.push_back(ns_stack.pop());
+      if (this->seen[this->get_current_namespace_prefix() + identifier]) {
+        global = this->get_current_namespace_prefix() + identifier;
         break;
       }
     }
@@ -64,14 +60,16 @@ public:
     return global.empty() ? identifier : global;
   }
 
-  void
-  maybe_resolve_type(std::optional<std::shared_ptr<ParseType::Type>> &type) {
-    if (type && (type.value()->tag == ParseType::USER_DEFINED ||
-                 type.value()->tag == ParseType::ARRAY)) {
-      auto identifier = type.value()->get_token().lexeme;
-      auto resolved = resolve_identifier(identifier);
-      if (seen[resolved]) {
-        set_type_token(type.value(), resolved);
+  void resolve_type(std::shared_ptr<ParseType::Type> &type) {
+    if (type && (type->tag == ParseType::USER_DEFINED ||
+                 type->tag == ParseType::ARRAY)) {
+      auto identifier = type->get_token().lexeme;
+      if (!this->is_primitive(identifier)) {
+        auto resolved = this->resolve_identifier(identifier);
+        auto token = type->get_token();
+        Token new_token(token.token_type, resolved, token.line_num,
+                        token.char_num);
+        type->set_token(new_token);
       }
     }
   }
@@ -80,25 +78,10 @@ public:
       std::vector<std::pair<Token, std::shared_ptr<ParseType::Type>>> &params) {
     for (auto &param : params) {
       if (param.first.lexeme != "self")
-        seen[param.first.lexeme] = true;
+        this->seen[param.first.lexeme] = true;
 
-      if (param.second->tag == ParseType::USER_DEFINED ||
-          param.second->tag == ParseType::ARRAY) {
-        auto identifier = param.second->get_token().lexeme;
-        // checks for array primitive
-        if (!is_primitive(identifier)) {
-          auto resolved = resolve_identifier(identifier);
-          set_type_token(param.second, resolved);
-        }
-      }
+      this->resolve_type(param.second);
     }
-  }
-
-  void set_type_token(std::shared_ptr<ParseType::Type> &type,
-                      const std::string &resolved) {
-    auto token = type->get_token();
-    Token new_token(token.token_type, resolved, token.line_num, token.char_num);
-    type->set_token(new_token);
   }
 
   std::string get_current_namespace_prefix() {
@@ -119,34 +102,36 @@ public:
 
   void visit_primary(Primary *primary) {
     if (primary->value.token_type == Token::Type::IDENTIFIER) {
-      auto resolved = resolve_identifier(primary->value.lexeme);
+      auto resolved = this->resolve_identifier(primary->value.lexeme);
       primary->value.lexeme = resolved;
     }
   }
 
   void visit_decl_stmt(DeclStmt *decl_stmt) {
     decl_stmt->identifier.lexeme =
-        get_current_namespace_prefix() + decl_stmt->identifier.lexeme;
+        this->get_current_namespace_prefix() + decl_stmt->identifier.lexeme;
 
-    maybe_resolve_type(decl_stmt->type);
+    if (decl_stmt->type)
+      this->resolve_type(decl_stmt->type.value());
 
-    seen[decl_stmt->identifier.lexeme] = true;
+    this->seen[decl_stmt->identifier.lexeme] = true;
     decl_stmt->value->accept(this);
   }
 
   void visit_const_stmt(ConstStmt *const_stmt) {
     const_stmt->identifier.lexeme =
-        get_current_namespace_prefix() + const_stmt->identifier.lexeme;
+        this->get_current_namespace_prefix() + const_stmt->identifier.lexeme;
 
-    maybe_resolve_type(const_stmt->type);
+    if (const_stmt->type)
+      this->resolve_type(const_stmt->type.value());
 
-    seen[const_stmt->identifier.lexeme] = true;
+    this->seen[const_stmt->identifier.lexeme] = true;
     const_stmt->value->accept(this);
   }
 
   void visit_struct_decl(StructDecl *struct_decl) {
     auto from_ns =
-        get_current_namespace_prefix() + struct_decl->identifier.lexeme;
+        this->get_current_namespace_prefix() + struct_decl->identifier.lexeme;
 
     if (!this->seen[from_ns]) {
       struct_decl->identifier.lexeme = from_ns;
@@ -159,7 +144,7 @@ public:
   }
 
   void visit_struct_initialization(StructInitialization *si) {
-    si->identifier.lexeme = resolve_identifier(si->identifier.lexeme);
+    si->identifier.lexeme = this->resolve_identifier(si->identifier.lexeme);
 
     for (auto &field : si->field_assignments) {
       field.second->accept(this);
@@ -168,38 +153,41 @@ public:
 
   void visit_type_stmt(TypeStmt *type_stmt) {
     type_stmt->identifier.lexeme =
-        get_current_namespace_prefix() + type_stmt->identifier.lexeme;
+        this->get_current_namespace_prefix() + type_stmt->identifier.lexeme;
 
-    seen[type_stmt->identifier.lexeme] = true;
+    this->seen[type_stmt->identifier.lexeme] = true;
   }
 
   void visit_func(Func *func) {
     func->identifier.lexeme =
-        get_current_namespace_prefix() + func->identifier.lexeme;
+        this->get_current_namespace_prefix() + func->identifier.lexeme;
 
-    decorate_param_list(func->param_list);
-    maybe_resolve_type(func->return_type);
+    this->decorate_param_list(func->param_list);
 
-    seen[func->identifier.lexeme] = true;
+    if (func->return_type)
+      this->resolve_type(func->return_type.value());
+
+    this->seen[func->identifier.lexeme] = true;
     func->block->accept(this);
   }
 
   void visit_method(Method *method) {
     method->class_identifier.lexeme =
-        get_current_namespace_prefix() + method->class_identifier.lexeme;
+        this->get_current_namespace_prefix() + method->class_identifier.lexeme;
 
-    decorate_param_list(method->param_list);
-    maybe_resolve_type(method->return_type);
+    this->decorate_param_list(method->param_list);
+    if (method->return_type)
+      this->resolve_type(method->return_type.value());
 
-    seen[method->identifier.lexeme] = true;
+    this->seen[method->identifier.lexeme] = true;
     method->block->accept(this);
   }
 
   void visit_assign_expr(AssignExpr *assign_expr) {
     assign_expr->identifier.lexeme =
-        resolve_identifier(assign_expr->identifier.lexeme);
+        this->resolve_identifier(assign_expr->identifier.lexeme);
 
-    seen[assign_expr->identifier.lexeme] = true;
+    this->seen[assign_expr->identifier.lexeme] = true;
     assign_expr->value->accept(this);
   }
 
@@ -214,16 +202,16 @@ public:
   void visit_scope_resolution(ScopeResolutionExpr *scope_resolution) {
     auto ns = scope_resolution->_namespace.lexeme + "::";
 
-    // need these checks for assignments, which might have a scope resolution on
-    // lhs and rhs causing double push
-    if (r_stack.empty() || r_stack.peek() != ns) {
-      r_stack.push(ns);
+    // need these checks for assignments, which might have a scope resolution
+    // on lhs and rhs causing double push
+    if (this->r_stack.empty() || this->r_stack.peek() != ns) {
+      this->r_stack.push(ns);
     }
 
     scope_resolution->identifier->accept(this);
 
-    if (!r_stack.empty() && r_stack.peek() == ns) {
-      r_stack.pop();
+    if (!this->r_stack.empty() && this->r_stack.peek() == ns) {
+      this->r_stack.pop();
     }
   }
 };
